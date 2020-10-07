8e26322b3b2ef545a08de8909fc18ff095dcb1dd
Ege Emir Özkan <egeemirozkan24@gmail.com>
2020-10-07 09:12:33 +0300

    Started the parser

--- internals/fluxion_parser.c
+++ internals/fluxion_parser.c
@@ -3,3 +3,65 @@
 //
 
 #include "fluxion_parser.h"
+#include <ctype.h>
+#include <string.h>
+
+TokenStack *initTokenStack() {
+    TokenStack  *stack = (TokenStack *) malloc(sizeof(TokenStack));
+    stack->current = 0;
+    stack->capacity = 8;
+    stack->tokens = (Token**) malloc(sizeof(Token) * stack->capacity);
+    return stack;
+}
+
+
+void StackPush(TokenStack *stack, Token *token) {
+    if (stack->current >= stack->capacity) {
+        stack->capacity *= 2;
+        stack->tokens = (Token**) realloc(stack->tokens, sizeof(Token*) * stack->capacity);
+    }
+    stack->tokens[stack->current++] = token;
+}
+
+Token *StackPop(TokenStack *stack, Token *token) {
+    if (stack->current == 0) {
+        return NULL;
+    } else {
+        return stack->tokens[stack->current--];
+    }
+}
+
+void parserConsume(Parser *parser) {
+    if (parser->ch_ != 0)
+        parser->ch_++;
+}
+
+char parserPeek(Parser *parser) {
+    return *parser->ch_;
+}
+char parserPop(Parser *parser) {
+    char c = parserPeek(parser);
+    parser->ch_++;
+    return c;
+}
+
+bool isDigit(Parser *parser) {
+    return isdigit(parserPeek(parser)) == 1;
+}
+
+bool isWhitespace(Parser *parser) {
+    return parserPeek(parser) == ' ' || parserPeek(parser) == '\t';
+}
+
+bool isEOL(Parser *parser) {
+    return parserPeek(parser) == '\n' && !parser->ignoreEOL;
+}
+
+NumberToken *parseNumber(Parser *parser) {
+    char *number = "";
+    while (!isEOL(parser) && !isWhitespace(parser) && (isDigit(parser) || parserPeek(parser) == '.')) {
+        strncat(number, parser->ch_, 1);
+        parserConsume(parser);
+    }
+    return initNumberToken(parser->lineCount, strtod(number, NULL));
+}
--- internals/fluxion_parser.h
+++ internals/fluxion_parser.h
@@ -4,10 +4,39 @@
 
 #ifndef FLUXIONCORE_FLUXION_PARSER_H
 #define FLUXIONCORE_FLUXION_PARSER_H
+#include "fluxion_token.h"
 
 
 typedef struct {
+    Token **tokens;
+    int current;
+    int capacity;
+} TokenStack;
 
-} FluxionParser;
+TokenStack *initTokenStack();
+void StackPush(TokenStack *stack, Token *token);
+Token *StackPop(TokenStack *stack, Token *token);
 
+typedef struct {
+    const char *source;
+    char *ch_;
+    bool ignoreEOL;
+    int lineCount;
+    TokenStack stack;
+} Parser;
+
+
+void parserConsume(Parser *parser);
+char parserPeek(Parser *parser);
+char parserPop(Parser *parser);
+
+bool isDigit(Parser *parser);
+bool isWhitespace(Parser *parser);
+bool isEOL(Parser *Parser);
+
+ExpressionToken *parseExpression(Parser *parser);
+NumberToken *parseNumber(Parser *parser);
+Parser *parse(const char *source);
+Token **getTokens(Parser *parser);
+int getTokenCount(Parser *parser);
 #endif //FLUXIONCORE_FLUXION_PARSER_H
